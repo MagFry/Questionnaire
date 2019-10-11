a3f251cca80c38873e02c4a6c0e2603ec67a8e1d
Magda <mfryczka96@gmail.com>
2019-10-11 22:23:29 +0200

    Adding Movie model

--- Movies/models.py
+++ Movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movies(models.Model):
+    movie_id = models.IntegerField(primary_key=True)
+    movie_id_tmdb = models.IntegerField()
+    movie_title = models.CharField(max_length=200)
