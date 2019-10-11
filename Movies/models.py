9bd6ed77941f75b61e5cd37771b7a302b6b9a626
Ewa Czechowska <ewa@ai-traders.com>
2019-10-12 01:00:56 +0200

    add functions to customly filter movies by genres with tests

--- Movies/models.py
+++ Movies/models.py
@@ -7,3 +7,41 @@ class Movies(models.Model):
     movie_id = models.IntegerField(primary_key=True)
     movie_id_tmdb = models.IntegerField()
     movie_title = models.CharField(max_length=200)
+    # comma separated list of genres, e.g. Comedy,Horror
+    movie_genres = models.CharField(max_length=800)
+
+
+    def __str__(self):
+        return self.movie_title
+
+
+    # returns an array of strings, e.g. ['Comedy', 'Horror']
+    def get_genres_as_array(self):
+        return self.movie_genres.split(',')
+
+
+    # returns True if this movie genres contains specific_genre
+    def is_of_specific_genre(self, specific_genre):
+        genres_array = self.get_genres_as_array()
+        return specific_genre in genres_array
+
+
+    # returns True if this movie genres exclude all the genres from specific_genres_arr
+    def excludes_specific_genres(self, specific_genres_arr):
+        genres_array = self.get_genres_as_array()
+        for genre in specific_genres_arr:
+            if genre in genres_array:
+                return False
+        return True
+
+    # return an array of movies which are of specific genre, but also
+    # their genres are not in excluded_genres_arr
+    # (this method will be used to produce different views, e.g.
+    # a view with movies that are romance, but not comedy and not horror)
+    def get_movies_by_genre(self, genre, excluded_genres_arr):
+        movies_in_db = Movies.objects.all()
+        movies_to_be_returned = []
+        for movie in movies_in_db:
+            if movie.is_of_specific_genre(genre) and movie.excludes_specific_genres(excluded_genres_arr):
+                movies_to_be_returned.append(movie)
+        return movies_to_be_returned
--- Movies/tests.py
+++ Movies/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from Movies.models import Movies
 
-# Create your tests here.
+# https://docs.djangoproject.com/en/2.2/topics/testing/overview/
+# https://docs.djangoproject.com/en/2.2/intro/tutorial05/
+
+class MoviesTestCase(TestCase):
+    def setUp(self):
+        Movies.objects.create(movie_id=1, movie_id_tmdb=1, movie_title="title1", movie_genres="Comedy,Horror")
+        Movies.objects.create(movie_id=2, movie_id_tmdb=2, movie_title="title2", movie_genres="Horror")
+        Movies.objects.create(movie_id=3, movie_id_tmdb=3, movie_title="title3", movie_genres="Thriller")
+
+    def test_movie_get_genres_as_array(self):
+        """Movie can return its genres as an array"""
+        movie1 = Movies.objects.get(movie_title="title1")
+        movie2 = Movies.objects.get(movie_title="title2")
+        self.assertEqual(movie1.get_genres_as_array(), ['Comedy', 'Horror'])
+        self.assertEqual(movie2.get_genres_as_array(), ['Horror'])
+
+    def test_movie_is_of_specific_genre(self):
+        movie1 = Movies.objects.get(movie_title="title1")
+        self.assertEqual(movie1.is_of_specific_genre('Comedy'), True)
+        self.assertEqual(movie1.is_of_specific_genre('Horror'), True)
+        self.assertEqual(movie1.is_of_specific_genre('NotExisting'), False)
+
+    def test_movie_excludes_specific_genres(self):
+        movie1 = Movies.objects.get(movie_title="title1")
+        self.assertEqual(movie1.excludes_specific_genres(['Comedy']), False)
+        self.assertEqual(movie1.excludes_specific_genres(['Horror']), False)
+        self.assertEqual(movie1.excludes_specific_genres(['Comedy','Horror']), False)
+        self.assertEqual(movie1.excludes_specific_genres(['Comedy','Horror','sth1']), False)
+        self.assertEqual(movie1.excludes_specific_genres(['Comedy','sth1']), False)
+        self.assertEqual(movie1.excludes_specific_genres(['sth1']), True)
+
+    def test_movie_get_movies_by_genre(self):
+        movie1 = Movies.objects.get(movie_title="title1")
+        movie2 = Movies.objects.get(movie_title="title2")
+        movie3 = Movies.objects.get(movie_title="title3")
+        self.assertEqual(movie1.get_movies_by_genre('Horror', []), [movie1,movie2])
+        self.assertEqual(movie1.get_movies_by_genre('sth1', []), [])
+        self.assertEqual(movie1.get_movies_by_genre('Horror', ['sth1']), [movie1,movie2])
+        self.assertEqual(movie1.get_movies_by_genre('Thriller', ['Horror','Comedy']), [movie3])
+        self.assertEqual(movie1.get_movies_by_genre('Thriller', []), [movie3])
