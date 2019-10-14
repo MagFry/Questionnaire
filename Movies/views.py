993974920e884f05114e0faec2faad1a2e6d357f
Ewa Czechowska <ewa@ai-traders.com>
2019-10-14 22:13:48 +0200

    print username on movies_list page

--- Movies/views.py
+++ Movies/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-file_path = 'Data/movies.csv'
-from numpy import loadtxt
-import pandas as pd
 from .models import Movies
 from django.shortcuts import render, redirect
 #from . import forms
@@ -13,6 +10,13 @@ from django.contrib.auth.decorators import login_required
 def movie_list(request):
     movie = Movies.objects.filter(movie_id=1)
     return render(request, 'movies/movies_list.html', {'movies':movie})
+    if request.method == "POST":
+        your_name = request.POST["your_name"]
+        movie = Movies.objects.filter(movie_id=1)
+        return render(request, 'movies/movies_list.html', {'movies':movie, 'your_name': your_name})
+    else:
+        movie = Movies.objects.filter(movie_id=1)
+        return render(request, 'movies/movies_list.html', {'movies':movie})
 
 def index(request):
     return render(request, 'movies/index.html')
