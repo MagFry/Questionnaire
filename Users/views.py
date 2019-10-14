69ccdf86da9d6cb42eccdd0149b0ef7a125fa9a4
Magda <mfryczka96@gmail.com>
2019-10-14 22:41:19 +0200

    Reparing get_name functionality

--- Users/views.py
+++ Users/views.py
@@ -1,22 +1,22 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-
-from .forms import NameForm
+from django.shortcuts import render,redirect
+from . import forms
 
 def get_name(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        username_form = NameForm(request.POST)
+        form = forms.NameForm(request.POST)
         # check whether it's valid:
-        if username_form.is_valid():
+        if form.is_valid():
             # save user to db
-            username_form.save()
+            instance = form.save(commit=False)
             # redirect to a new URL:
-            return HttpResponseRedirect('movies/movies_list.html')
+            instance.save()
+            return redirect('movie:list')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = NameForm()
+        form = forms.NameForm()
 
     return render(request, 'users/get_name.html', {'form': form})
