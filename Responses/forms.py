7a27979329a19fd1e5148f7cf92de12df57f3a0a
Magda <mfryczka96@gmail.com>
2019-10-15 11:19:15 +0200

    adding responses form

---
+++ Responses/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . import models
+
+class VoteForm(forms.ModelForm):
+    class Meta:
+        model = models.Responses
+        fields = ['movie_id', 'user_id' 'user_rate']
+
+
--- Responses/models.py
+++ Responses/models.py
@@ -6,9 +6,10 @@ from Users.models import Users
 
 
 class Responses(models.Model):
-    respond_id = models.IntegerField(primary_key=True)
+    respond_id = models.AutoField(primary_key=True)
     movie_id = models.ForeignKey(Movies, on_delete=models.CASCADE)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user_rate = models.IntegerField()
 
     def __str__(self):
         return self.respond_id
