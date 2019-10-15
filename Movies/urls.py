3114b18e37be01fee3a4e8928fb6f95f82e952e6
Ewa Czechowska <ewa@ai-traders.com>
2019-10-15 10:59:07 +0200

    use 1 movies category html template

--- Movies/urls.py
+++ Movies/urls.py
@@ -6,7 +6,9 @@ app_name = 'movie'
 
 urlpatterns = [
     url(r'^movie_list/$', views.movie_list, name='list'),
-    url(r'^horror_movies/$', views.horror_movies, name='horror'),
-    url(r'^mystery_movies/$', views.mystery_movies, name='mystery'),
+    #url(r'^horror_movies/$', views.horror_movies, name='horror'),
+    #url(r'^mystery_movies/$', views.mystery_movies, name='mystery'),
+    url(r'^collect_ratings/$', views.collect_ratings, name='collect_ratings'),
+    url(r'^first_movies_category/$', views.first_movies_category, name='first_movies_category'),
     url(r'^index/$', views.index, name='index'),
 ]
