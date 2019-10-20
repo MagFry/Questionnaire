7d4b8f7c67a21b334c45787c79ddd3ad1877b6d1
Ewa Czechowska <ewa@ai-traders.com>
2019-10-20 09:22:20 +0200

    allow all hosts

--- Questionnaire/settings.py
+++ Questionnaire/settings.py
@@ -54,7 +54,7 @@ SECRET_KEY = '9nzyfd6=5#v9g&yrx&e_nlmgqk!7rnhy^36gjr^8bdhxkde$nx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
