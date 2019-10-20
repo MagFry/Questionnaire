2202aba1aecc51699cb11e6c507dfc0f550b803a
Magda <mfryczka96@gmail.com>
2019-10-20 23:29:45 +0200

    upgrading get_csv function to display only users which rate all movies

--- Responses/views.py
+++ Responses/views.py
@@ -4,14 +4,21 @@ from Users.models import Users
 from django.http import HttpResponse
 
 def get_csv(request):
+    users = Users.objects.all()
+    imp_users = []
+    for user in users:
+        responses_for_one_user = Responses.objects.all().filter(user_id=user.user_id)
+        if len(responses_for_one_user) == 200:
+            imp_users.append(user.user_id)
     responses = Responses.objects.all()
     csv = "respond_id,user_id,user_name,movie_id,movie_title,user_rate"
     for response in responses:
-        line = str(response.respond_id) + ',' \
-             + str(response.user_id.user_id) + ',' \
-             + str(response.user_id.user_name) + ',' \
-             + str(response.movie_id.movie_id) + ',' \
-             + str(response.movie_id.movie_title) + ',' \
-             + str(response.user_rate)
-        csv = csv + "<br>" + line
+        if response.user_id.user_id in imp_users:
+            line = str(response.respond_id) + ',' \
+                 + str(response.user_id.user_id) + ',' \
+                 + str(response.user_id.user_name) + ',' \
+                 + str(response.movie_id.movie_id) + ',' \
+                 + str(response.movie_id.movie_title) + ',' \
+                 + str(response.user_rate)
+            csv = csv + "<br>" + line
     return HttpResponse(csv)
